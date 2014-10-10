bf2e7dc057ce5b29f15751ef7f277d90baa6057a
Dan Stutzman <dan@danstutzman.com>
2014-10-10 14:25:40 -0600

    Serve an alert message instead of browserified.js if browserified.js isn't newer than all the files under app/ (indicating a compile error).

--- dev-server.js
+++ dev-server.js
@@ -2,12 +2,70 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
-http.createServer(function (request, response) {
-  var filePath = '.' + request.url;
-  if (filePath == './') {
-    filePath = './index.html';
-  }
+var walk = function (dir, handleFile, done) {
+    fs.readdir(dir, function (error, list) {
+        if (error) {
+            return done(error);
+        }
 
+        var i = 0;
+
+        (function next () {
+            var file = list[i++];
+
+            if (!file) {
+                return done(null);
+            }
+
+            file = dir + '/' + file;
+
+            fs.stat(file, function (error, stat) {
+                if (stat && stat.isDirectory()) {
+                    walk(file, handleFile, function (error) {
+                        next();
+                    });
+                } else {
+                    try {
+                        handleFile(file, stat, next);
+                    } catch (e) {
+                        done(e);
+                    }
+                }
+            });
+        })();
+    });
+};
+
+function browserifiedServe(filePath, request, response) {
+  fs.stat(filePath, function(error, stat) {
+    var compiledMtime = stat.mtime;
+    function complainIfCoffeeTimestampTooLate(path, stat, callback) {
+      if (compiledMtime < stat.mtime) {
+        throw { message: "Path " + path + " is newer than " + filePath + "!" };
+      }
+      else {
+        callback();
+      }
+    }
+    walk('../app/', complainIfCoffeeTimestampTooLate, function(error) {
+      if (error) {
+        console.error(error);
+        var content = "alert('" + JSON.stringify(error) + "');";
+        console.log('200 ' + request.url);
+        response.writeHead(200, { 'Content-Type': 'text/javascript' });
+        response.end(content, 'utf-8');
+      } else {
+        fs.readFile(filePath, function(error, content) {
+          console.log('200 ' + request.url);
+          response.writeHead(200, { 'Content-Type': 'text/javascript' });
+          response.end(content, 'utf-8');
+        });
+      }
+    });
+  });
+}
+
+function normalServe(filePath, request, response) {
   var extname = path.extname(filePath);
   var contentType = 'text/html';
   switch (extname) {
@@ -18,7 +76,7 @@ http.createServer(function (request, response) {
     case '.jpeg': contentType = 'image/jpeg';               break;
     case '.ttf':  contentType = 'application/octet-stream'; break;
   }
-  
+
   fs.exists(filePath, function(exists) {
     if (exists) {
       fs.readFile(filePath, function(error, content) {
@@ -45,7 +103,21 @@ http.createServer(function (request, response) {
 //      response.end('404', 'utf-8');
     }
   });
-  
+}
+
+http.createServer(function (request, response) {
+  var filePath = '.' + request.url;
+  if (filePath == './') {
+    filePath = './index.html';
+  }
+
+  if (request.url == '/javascripts/browserified.js') {
+    browserifiedServe(filePath, request, response);
+  }
+  else {
+    normalServe(filePath, request, response);
+  }
+
 }).listen(8000);
 
 console.log('Server running at http://127.0.0.1:8000/');
