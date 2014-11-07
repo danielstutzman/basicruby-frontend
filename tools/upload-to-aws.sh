cce067bdfb55a7480e278a4e40882ad9ab803df4
Dan Stutzman <dan@danstutzman.com>
2014-11-07 14:41:01 -0700

    Explicitly set max-age to 0 for HTML pages; better comments.

--- tools/upload-to-aws.sh
+++ tools/upload-to-aws.sh
@@ -6,28 +6,25 @@ s3cmd ws-create s3://basicruby-frontend-danstutzman --ws-error=404
 
 #s3cmd del --recursive s3://basicruby-frontend-danstutzman --force
 
-# First, gzip .html and .js and .css files
-# note: ending \; is required
-
-# Sync some top-level files
+# Sync some top-level files with forced revalidation
 for FILE in index.html test.html robots.txt sitemap.txt; do
   ../tools/gzip_if_not_gzipped.sh $FILE
 done
-s3cmd sync . --exclude="*" --rinclude="^(index.html|test.html|sitemap.txt|robots.txt)$" s3://basicruby-frontend-danstutzman --acl-public --add-header 'Content-Encoding:gzip'
+s3cmd sync . --exclude="*" --rinclude="^(index.html|test.html|sitemap.txt|robots.txt)$" s3://basicruby-frontend-danstutzman --acl-public --add-header 'Content-Encoding:gzip' --add-header="Cache-Control:max-age=0"
 
-# Sync gzipped and versioned files
+# Sync gzipped and versioned files with 1 year expiration
 find . -iname '*.js'   -exec ../tools/gzip_if_not_gzipped.sh {} \;
 find . -iname '*.css'  -exec ../tools/gzip_if_not_gzipped.sh {} \;
 s3cmd sync . --exclude="*" --rinclude="[0-9a-f]{16}.(js|css)" s3://basicruby-frontend-danstutzman --acl-public --add-header="Cache-Control:max-age=31536000" --add-header 'Content-Encoding:gzip'
 
-# Sync non-gzipped but version filed (e.g. images)
+# Sync non-gzipped but version filed (e.g. images) with 1 year expiration
 s3cmd sync . --exclude="*" --rinclude="[0-9a-f]{16}" s3://basicruby-frontend-danstutzman --acl-public --add-header="Cache-Control:max-age=31536000"
 
-# Sync HTML and gzipped files under static*
+# Sync HTML and gzipped files under static* with forced revalidation
 find static* -type f -exec ../tools/gzip_if_not_gzipped.sh {} \;
 for DIR in static*; do
   cd $DIR
-  s3cmd sync . --add-header 'Content-Encoding:gzip' s3://basicruby-frontend-danstutzman --acl-public --default-mime-type=text/html
+  s3cmd sync . --add-header 'Content-Encoding:gzip' --add-header "Cache-Control:max-age=0" s3://basicruby-frontend-danstutzman --acl-public --default-mime-type=text/html
   cd ..
 done
 
