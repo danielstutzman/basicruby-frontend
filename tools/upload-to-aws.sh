31f65506e34b1a3554fc02ec8aba0c99bfaad854
Dan Stutzman <dan@danstutzman.com>
2014-11-14 17:29:50 -0700

    Un-hard-code S3 bucket name.

--- tools/upload-to-aws.sh
+++ tools/upload-to-aws.sh
@@ -1,34 +1,41 @@
 #!/bin/bash -e
 cd `dirname $0`/../dist
 
-aws s3 mb s3://basicruby-frontend-danstutzman
-s3cmd ws-create s3://basicruby-frontend-danstutzman --ws-error=404
+if [ "$1" == "" ]; then
+  echo 1>&2 "First arg must be bucket name"
+  exit 1
+fi
+BUCKET_NAME="$1"
+BUCKET_URL="s3://$BUCKET_NAME"
 
-#s3cmd del --recursive s3://basicruby-frontend-danstutzman --force
+aws s3 mb $BUCKET_URL
+s3cmd ws-create $BUCKET_URL --ws-error=404
+
+#s3cmd del --recursive $BUCKET_URL --force
 
 # Sync favicon.ico with 1 year expiration
-s3cmd put favicon.ico s3://basicruby-frontend-danstutzman/favicon.ico --acl-public --add-header="Cache-Control:public,max-age=31536000"
+s3cmd put favicon.ico $BUCKET_URL/favicon.ico --acl-public --add-header="Cache-Control:public,max-age=31536000"
 
 # Sync some top-level files with forced revalidation
 for FILE in index.html test.html robots.txt sitemap.txt; do
   ../tools/gzip_if_not_gzipped.sh $FILE
 done
-s3cmd sync . --exclude="*" --rinclude="^(index.html|test.html|sitemap.txt|robots.txt)$" s3://basicruby-frontend-danstutzman --acl-public --add-header 'Content-Encoding:gzip' --add-header="Cache-Control:public,max-age=0"
+s3cmd sync . --exclude="*" --rinclude="^(index.html|test.html|sitemap.txt|robots.txt)$" $BUCKET_URL --acl-public --add-header 'Content-Encoding:gzip' --add-header="Cache-Control:public,max-age=0"
 
 # Sync gzipped and versioned files with 1 year expiration
 find . -iname '*.js'   -exec ../tools/gzip_if_not_gzipped.sh {} \;
 find . -iname '*.css'  -exec ../tools/gzip_if_not_gzipped.sh {} \;
-s3cmd sync . --exclude="*" --rinclude="[0-9a-f]{16}.(js|css)" s3://basicruby-frontend-danstutzman --acl-public --add-header="Cache-Control:public,max-age=31536000" --add-header 'Content-Encoding:gzip'
+s3cmd sync . --exclude="*" --rinclude="[0-9a-f]{16}.(js|css)" $BUCKET_URL --acl-public --add-header="Cache-Control:public,max-age=31536000" --add-header 'Content-Encoding:gzip'
 
 # Sync non-gzipped but version filed (e.g. images) with 1 year expiration
-s3cmd sync . --exclude="*" --rinclude="[0-9a-f]{16}" s3://basicruby-frontend-danstutzman --acl-public --add-header="Cache-Control:public,max-age=31536000"
+s3cmd sync . --exclude="*" --rinclude="[0-9a-f]{16}" $BUCKET_URL --acl-public --add-header="Cache-Control:public,max-age=31536000"
 
 # Sync HTML and gzipped files under static* with forced revalidation
 find static* -type f -exec ../tools/gzip_if_not_gzipped.sh {} \;
 for DIR in static*; do
   cd $DIR
-  s3cmd sync . --add-header 'Content-Encoding:gzip' --add-header "Cache-Control:public,max-age=0" s3://basicruby-frontend-danstutzman --acl-public --default-mime-type=text/html
+  s3cmd sync . --add-header 'Content-Encoding:gzip' --add-header "Cache-Control:public,max-age=0" $BUCKET_URL --acl-public --default-mime-type=text/html
   cd ..
 done
 
-open http://basicruby-frontend-danstutzman.s3-website-us-east-1.amazonaws.com
+open http://$BUCKET_NAME.s3-website-us-east-1.amazonaws.com
